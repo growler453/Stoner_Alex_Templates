ef09617d0662774750d531ad53c3aec8f6b1aafd
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 01:54:57 -0500

    Added the driver and implemented the half function to use static cast.

--- Stoner_Alex_Templates/Templates.cpp
+++ Stoner_Alex_Templates/Templates.cpp
@@ -2,24 +2,29 @@
 // CIS 1202 502
 // December 5, 2020
 
-#include <iostream>
+#include<iostream>
+#include<cmath>
 using namespace std;
 
 template<class T>
 T half(T num) {
-	return num;
+	return num /= 2.0f;
 }
 
 int half(int);
 
 int main() {
-	double a;
-	float b;
-	int c;
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
 
 	return 0;
 }
 
 int half(int i) {
-
+	return round(static_cast<float>(i) / 2.0f);
 }
