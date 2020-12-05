fa03dd48e8ffad883637d10fe16a81cd4d41fa10
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 01:45:07 -0500

    Add project files.

---
+++ Stoner_Alex_Templates/Templates.cpp
@@ -0,0 +1,25 @@
+// Alex Stoner
+// CIS 1202 502
+// December 5, 2020
+
+#include <iostream>
+using namespace std;
+
+template<class T>
+T half(T num) {
+	return num;
+}
+
+int half(int);
+
+int main() {
+	double a;
+	float b;
+	int c;
+
+	return 0;
+}
+
+int half(int i) {
+
+}
